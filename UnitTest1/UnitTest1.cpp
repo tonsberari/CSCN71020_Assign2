65dc1e4b872d3761d3e99109c47be92c7734dc4e
t3kkerz <t.edmundjaka7@gmail.com>
2024-10-17 23:02:09 +0100

    failed test

--- UnitTest1/UnitTest1.cpp
+++ UnitTest1/UnitTest1.cpp
@@ -7,6 +7,8 @@ extern "C"
 
 int getPerimeter(int* length, int* width);
 int getArea(int* length, int* width);
+void setLength(int input, int* length);
+void setWidth(int input, int* width);
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -21,11 +23,11 @@ namespace UnitTest1
 			int length = 15;
 			int width = 20;
 
-			int expectedvalue = 70;
+			int expectedperimeter = 70;
 
 			int result = getPerimeter(&length, &width);
 
-			Assert::AreEqual(result, expectedvalue);
+			Assert::AreEqual(result, expectedperimeter);
 		}
 
 		TEST_METHOD(TEST_GET_AREA) {
@@ -36,6 +38,53 @@ namespace UnitTest1
 
 			int result = getArea(&length, &width);
 			Assert::AreEqual(result, expectedvalue);
+		}
+		TEST_METHOD(TestSetLenth_Valid) {
+			int input = 20;
+			int length = 0;
+
+			setLength(input, &length);
+			Assert::AreEqual(input, length);
+		}
+
+		TEST_METHOD(TestSetLenth_Lower) {
+			int input = 10;
+			int length = -5;
+
+			setLength(input, &length);
+			Assert::AreEqual(input, length);
+		}
+
+		TEST_METHOD(TestSetLenth_Higher) {
+			int input = 30;
+			int length = 105;
+
+			setLength(input, &length);
+			Assert::AreEqual(input, length);
+		}
+
+		TEST_METHOD(TestSetWidth_Valid) {
+			int input = 20;
+			int width = 0;
+
+			setLength(input, &width);
+			Assert::AreEqual(input, width);
+		}
+
+		TEST_METHOD(TestSetWidth_Lower) {
+			int input = 30;
+			int width = -5;
+
+			setLength(input, &width);
+			Assert::AreEqual(input, width);
+		}
+
+		TEST_METHOD(TestSetWidth_Higher) {
+			int input = 40;
+			int width = 105;
+
+			setLength(input, &width);
+			Assert::AreEqual(input, width);
 		}
 	};
 }
